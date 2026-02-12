c5ced580caf0db22a7fe5cf68441f86e30db0772
decobot <capy@deco.cx>
2026-02-12 16:04:00 -0300

    feat: add gtm events

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Menu } from "@/app/components/Menu/Menu";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -62,6 +63,8 @@ export const metadata: Metadata = {
   },
 };
 
+const GTM_ID=process.env.NEXT_PUBLIC_GTM_ID
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -69,6 +72,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <Script
+  id="gtm-base"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${GTM_ID}');
+    `,
+  }}
+/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
