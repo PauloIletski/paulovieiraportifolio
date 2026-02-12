264f687f1b9e99c4dca6512e83d77a84528fb726
decobot <capy@deco.cx>
2026-02-12 11:34:45 -0300

    feat: adjust SEO image

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://paulovieira.dev"),
+  metadataBase: new URL("https://paulovieira.site"),
   title: {
     default: "Paulo Vieira | Desenvolvedor Full Stack",
     template: "%s | Paulo Vieira",
